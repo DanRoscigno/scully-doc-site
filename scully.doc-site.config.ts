313c9dc0be0d2c03684815372066d81b7c22360d
Natalia Venditto <natalia.venditto@netcentric.biz>
2020-07-07 09:30:24 +0200

    add initial styles

--- scully.doc-site.config.ts
+++ scully.doc-site.config.ts
@@ -1,14 +1,19 @@
 import { ScullyConfig } from '@scullyio/scully';
+const { DisableAngular } = require('scully-plugin-disable-angular');
+
+const postRenderers = [DisableAngular];
+
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "doc-site",
+  projectRoot: './src',
+  projectName: 'doc-site',
   outDir: './dist/static',
   routes: {
     '/docs/:slug': {
       type: 'contentFolder',
       slug: {
-        folder: "./docs"
-      }
+        folder: './docs'
+      },
+      postRenderers: postRenderers, 
     },
   }
 };
